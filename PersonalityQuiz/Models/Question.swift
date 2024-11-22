52e40a10d0448ed5bf09f803c85a91f1438984f5
Mitali Ahir <mitaliahir3@gmail.com>
2024-11-22 15:31:39 -0500

    Updated questions and input controlls with Question instance. Progress View updated.

--- PersonalityQuiz/Models/Question.swift
+++ PersonalityQuiz/Models/Question.swift
@@ -17,3 +17,31 @@ struct Question{
 enum ResponseType{
     case single, multiple, ranged
 }
+
+var questions: [Question] = [
+    Question(text: "Which food do you like the most?",
+             type: .single,
+             answers: [Answer(text: "Steak", type: .lion),
+                       Answer(text: "Fish", type: .cat),
+                       Answer(text: "Carrots", type: .rabbit),
+                       Answer(text: "Lettus", type: .turtle)
+                      ]
+            ),
+    Question(text: "Which activities do you enjoy?",
+             type: .multiple,
+             answers: [Answer(text: "Eating", type: .lion),
+                       Answer(text: "Sleeping", type: .cat),
+                       Answer(text: "Cuddling", type: .rabbit),
+                       Answer(text: "Swimming", type: .turtle)
+                      ]
+            ),
+    Question(text: "How much do you enjoy car rides?",
+             type: .ranged,
+             answers: [Answer(text: "I love them", type: .lion),
+                       Answer(text: "I dislike them", type: .cat),
+                       Answer(text: "I get little nervous", type: .rabbit),
+                       Answer(text: "I barely notice them", type: .turtle)
+                      ]
+            )
+]
+    
--- PersonalityQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalityQuiz/ViewControllers/QuestionViewController.swift
@@ -8,22 +8,84 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+    @IBOutlet weak var questionlabel: UILabel!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet weak var singleButton1: UIButton!
+    @IBOutlet weak var singleButton2: UIButton!
+    @IBOutlet weak var singleButton3: UIButton!
+    @IBOutlet weak var singleButton4: UIButton!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var multiLabel1: UILabel!
+    @IBOutlet weak var multiLabel2: UILabel!
+    @IBOutlet weak var multiLabel3: UILabel!
+    @IBOutlet weak var multiLabel4: UILabel!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var rangedLabel1: UILabel!
+    @IBOutlet weak var rangedLabel2: UILabel!
+    
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    var questionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updatedUI()// Set visibility of views and update views
+        
+    }
+    func updatedUI(){
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
+        
+        //Updating navigation item title to particular index of question
+        navigationItem.title = "Question \(questionIndex + 1)"
+        
+        //Using questionIndex property to access particular question
+        let currentQuestion = questions[questionIndex]
+        questionlabel.text = currentQuestion.text
+        
+        let progressValue = Float(questionIndex) / Float(questions.count)
+        questionProgressView.setProgress(progressValue, animated: true)
+        
+        let currentAnswers = currentQuestion.answers
+        
+        //Showing input view depending on question type
+        switch currentQuestion.type {
+        case .single:
+            updateSingleStack(using: currentAnswers)
+        case .multiple:
+            updateMultiStack(using: currentAnswers)
+        case .ranged:
+            updateRangedStack(using: currentAnswers)
+        }
+      
+        
+    }//updateUI() ends
+    
+    func updateSingleStack(using answers: [Answer]){
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+        //singleButton1.setTitle(currentQuestion.answers[0].text, for: .normal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateMultiStack(using answers: [Answer]){
+        multipleStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
     }
-    */
 
+    func updateRangedStack(using answers: [Answer]){
+        rangedStackView.isHidden = false
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
+        
+    }
 }
