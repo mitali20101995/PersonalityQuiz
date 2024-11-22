1f3573e873823514144701bc0514ed076d3c6083
Mitali Ahir <mitaliahir3@gmail.com>
2024-11-22 17:39:27 -0500

    Captured answers for each type of question and saved it to the list.

--- PersonalityQuiz/ViewControllers/QuestionViewController.swift
+++ PersonalityQuiz/ViewControllers/QuestionViewController.swift
@@ -21,20 +21,76 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multiLabel2: UILabel!
     @IBOutlet weak var multiLabel3: UILabel!
     @IBOutlet weak var multiLabel4: UILabel!
+    @IBOutlet weak var multiSwitch1: UISwitch!
+    @IBOutlet weak var multiSwitch2: UISwitch!
+    @IBOutlet weak var multiSwitch3: UISwitch!
+    @IBOutlet weak var multiSwitch4: UISwitch!
     
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet weak var questionProgressView: UIProgressView!
     
     var questionIndex = 0
-
+    var answersChosen: [Answer] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         updatedUI()// Set visibility of views and update views
         
     }
+    
+    //sender type is UIButton so that can specify which button was pressed for single type question stack view
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        //print(sender.titleLabel?.text ?? "nothing")
+        switch sender{
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            return
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswersButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        //print("Choices submitted.")
+        if multiSwitch1.isOn{
+            answersChosen.append(currentAnswers[0])
+        }
+        if multiSwitch2.isOn{
+            answersChosen.append(currentAnswers[1])
+        }
+        if multiSwitch3.isOn{
+            answersChosen.append(currentAnswers[2])
+        }
+        if multiSwitch4.isOn{
+            answersChosen.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
+    }
+   
+    
+    @IBAction func rangedAnswerButtonPressed() {
+        let currentAnswers = questions[questionIndex].answers
+        let index = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        answersChosen.append(currentAnswers[index])
+        
+        nextQuestion()
+    }
+    
+    
+    
     func updatedUI(){
         singleStackView.isHidden = true
         multipleStackView.isHidden = true
@@ -86,6 +142,11 @@ class QuestionViewController: UIViewController {
         rangedStackView.isHidden = false
         rangedLabel1.text = answers.first?.text
         rangedLabel2.text = answers.last?.text
+        
+    }
+    
+    func nextQuestion(){
+        
         
     }
 }
